c9768a177439ffeec5f8c56de8257297df674155
Axel irias <axelirias@rocketmail.com>
2022-05-07 10:47:04 -0600

    New script.js click event

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ h2.textContent = "A Little about me";
 document.querySelector("body").appendChild(h2)
 
 const button = document.querySelector('button')
-
 button.addEventListener('click', event => {
     button.textContent = `Click count ${event.detail}`;
 });
